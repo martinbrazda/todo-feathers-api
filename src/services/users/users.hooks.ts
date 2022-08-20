b65c1b03f9f2e71e0535ea48d8624cfea4c86b7d
martinbrazda <martinbrazda9@gmail.com>
2022-08-20 21:47:00 +0200

    + input validation to users service hooks

--- src/services/users/users.hooks.ts
+++ src/services/users/users.hooks.ts
@@ -1,23 +1,30 @@
 import * as feathersAuthentication from "@feathersjs/authentication";
 import * as local from "@feathersjs/authentication-local";
+import Joi from "joi";
+import validate from "feathers-validate-joi";
 // Don't remove this comment. It's needed to format import lines nicely.
 
 const { authenticate } = feathersAuthentication.hooks;
 const { hashPassword, protect } = local.hooks;
+
+const userCreateSchema = Joi.object().keys({
+  username: Joi.string().min(3).max(64).required(),
+  password: Joi.string().min(3).max(64).required(),
+});
 
 export default {
   before: {
     all: [],
     find: [ authenticate("jwt") ],
     get: [ authenticate("jwt") ],
-    create: [ hashPassword("password") ],
-    update: [ hashPassword("password"),  authenticate("jwt") ],
-    patch: [ hashPassword("password"),  authenticate("jwt") ],
+    create: [ validate.form(userCreateSchema), hashPassword("password") ],
+    update: [ validate.form(userCreateSchema), hashPassword("password"), authenticate("jwt") ],
+    patch: [ validate.form(userCreateSchema), hashPassword("password"), authenticate("jwt") ],
     remove: [ authenticate("jwt") ]
   },
 
   after: {
-    all: [ 
+    all: [
       // Make sure the password field is never sent to the client
       // Always must be the last hook
       protect("password")
